1eae3d0cadc4fef55742b03ae598dac7055ae0ea
Allen Mathews <allenjoemathews@gmail.com>
2022-06-29 14:40:31 +1000

    added changes to server side code from ibm coursera page

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import './bootstrap.min.css'
+import React from 'react';
+import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
-  render() {
-    //Returns the emotions as an HTML table
-    return (
-      <div>
-        <table className="table table-bordered">
-          <tbody>
-            {' '}
+    render() {
+      //Returns the emotions as an HTML table
+      return (  
+        <div>
+          <table className="table table-bordered">
+            <tbody>
             {
+                Object.entries(this.props.emotions).map(function(mapentry) {
+                    return (
+                        <tr>
+                        <td>{mapentry[0]}</td>
+                        <td>{mapentry[1]}</td>
+                        </tr>
+                    )
+                    })
               /*Write code to use the .map method that you worked on in the 
-                Hands-on React lab to extract the emotions. If you are stuck,
-                please click the instructions to see how to implement a map*/
-              Object.entries(this.props.emotions).map(function (mapentry) {
-                return (
-                  <tr>
-                    <td> {mapentry[0]} </td> <td> {mapentry[1]} </td>{' '}
-                  </tr>
-                )
-              })
-            }{' '}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+              Hands-on React lab to extract the emotions. If you are stuck,
+              please click the instructions to see how to implement a map*/
+            }
+            </tbody>
+          </table>
+          </div>
+          );
+        }
+    
 }
-export default EmotionTable
+export default EmotionTable;
